6db738848125031aaca49bf258b177e40c549eba
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-01 16:25:40 +0600

    Practice Writing File Asynchronously In JavaScript

--- NodeJs Core/write-file.js
+++ NodeJs Core/write-file.js
@@ -9,3 +9,13 @@ try {
 } catch (err) {
     console.error('Error writing file:', err.message);
 }
+
+// Asynchronous write
+const content2 = 'This is another content \nAsynchronous write in NodeJs!';
+fs.writeFile('./output/test-async.txt', content2, (err) => {
+    if (err) {
+        console.error('Error writing file:', err.message);
+    } else {
+        console.log('file written asynchronously')
+    }
+});
