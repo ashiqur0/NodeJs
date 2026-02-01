14bcf1a7c915d8b61a9210d8ff772525b86fcff0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-01 15:17:07 +0600

    Practice Common Js: Module Exports

--- file1.js
+++ file1.js
@@ -1,4 +1,8 @@
 const { x: a } = require('./file2');
 const { x } = require('./file3');
-
+const { add } = require('./utils/add');
+const { subtract } = require('./utils/subtract');
 // console.log(x, a);
+
+console.log('add: ', add(x, a));
+console.log('subtract: ', subtract(a, x));
