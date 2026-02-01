54a04578ffe8c24bdccb8274491096249ef0f78e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-01 15:51:20 +0600

    Practice Reading File Synchronously In JavaScript

--- NodeJs Core/read-sync.js
+++ NodeJs Core/read-sync.js
@@ -2,5 +2,12 @@ const fs = require('fs');
 
 console.log('Start reading...');
 
-const data = fs.readFileSync('./data/diary.txt', 'utf8');
-console.log(data);
+try {
+    const data = fs.readFileSync('./data/diary.txt', 'utf8');
+    console.log("File Content: ")
+    console.log(data);
+} catch (err) {
+    console.error('Error reading file:', err.message);
+}
+
+console.log("Finished reading...");
