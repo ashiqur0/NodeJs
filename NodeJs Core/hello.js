07f3c0a24db067364dc54a484923669c40a1ca80
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-01 15:37:58 +0600

    Add initial diary entries and user data, along with greeting and system info scripts

---
+++ NodeJs Core/hello.js
@@ -0,0 +1,6 @@
+console.log("node version: ", process.version);
+console.log("platform: ", process.platform);
+console.log("architecture: ", process.arch);
+console.log("current working directory: ", process.cwd());
+console.log("process ID: ", process.pid);
+console.log("uptime (seconds): ", process.uptime());
