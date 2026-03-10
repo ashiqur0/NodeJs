84db99f2cba114c01eef5ce3895ac2fe56aa41db
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-10 07:53:33 +0600

    Export RouteHandler type and routes map; refactor server to utilize addRoutes for handling requests

--- NodeJs/src/server.ts
+++ NodeJs/src/server.ts
@@ -1,30 +1,55 @@
 import http, { Server, IncomingMessage, ServerResponse } from 'http';
 import path from 'path';
 import config from './config';
+import addRoutes, { RouteHandler, routes } from './helpers/RouteHandler';
+
+addRoutes('GET', '/', (req, res) => {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({
+        message: "Hello from node js with TypeScript!",
+        path: req.url
+    }));
+});
 
 const server: Server = http.createServer(
     (req: IncomingMessage, res: ServerResponse) => {
 
-        // root route
-        if (req.url == '/' && req.method == 'GET') {
-            res.writeHead(200, { "content-type": "application/json" });
+        const method = req.method?.toUpperCase() || '';
+        const path = req.url || '';
+        const methodMap = routes.get(method);
+        const handler: RouteHandler | undefined = methodMap?.get(path);
+
+        if (handler) {
+            handler(req, res);
+        } else {
+            res.writeHead(404, { "content-type": "application/json" });
             res.end(JSON.stringify({
-                message: "Hello from node js with Typescript!",
-                path: req.url
+                success: false,
+                message: "Route not found",
+                path,
             }));
         }
+
+        // root route
+        // if (req.url == '/' && req.method == 'GET') {
+        //     res.writeHead(200, { "content-type": "application/json" });
+        //     res.end(JSON.stringify({
+        //         message: "Hello from node js with Typescript!",
+        //         path: req.url
+        //     }));
+        // }
 
         // health route
-        if (req.url == '/api' && req.method == 'GET') {
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(JSON.stringify({
-                message: "Health status is OK!",
-                path: req.url
-            }));
-        }
+        // if (req.url == '/api' && req.method == 'GET') {
+        //     res.writeHead(200, { "content-type": "application/json" });
+        //     res.end(JSON.stringify({
+        //         message: "Health status is OK!",
+        //         path: req.url
+        //     }));
+        // }
 
         // users route
-        if (req.url == "/api/users" && req.method == 'POST') {
+        // if (req.url == "/api/users" && req.method == 'POST') {
             // const user = {
             //     id: 9,
             //     name: "Ashiqur Rahman",
@@ -32,26 +57,26 @@ const server: Server = http.createServer(
             // res.writeHead(200, {"content-type": "application/json"});
             // res.end(JSON.stringify(user));
 
-            let body = '';
+        //     let body = '';
 
-            // listen for data chunk
-            req.on('data', (chunk) => {
-                body += chunk.toString();
-            });
+        //     // listen for data chunk
+        //     req.on('data', (chunk) => {
+        //         body += chunk.toString();
+        //     });
 
-            req.on('end', () => {
-                try {
-                    const parseBody = JSON.parse(body);
-                    console.log(parseBody);
-                    console.log('catching current changes');
-                    // res.end(body);
-                    res.end(JSON.stringify(parseBody));
-                } catch (error: any) {
-                    console.log(error?.message);
-                }
-            });
+        //     req.on('end', () => {
+        //         try {
+        //             const parseBody = JSON.parse(body);
+        //             console.log(parseBody);
+        //             console.log('catching current changes');
+        //             // res.end(body);
+        //             res.end(JSON.stringify(parseBody));
+        //         } catch (error: any) {
+        //             console.log(error?.message);
+        //         }
+        //     });
 
-        }
+        // }
     }
 );
 
